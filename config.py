62adb7a5164cbee7574e94013a4ee667d2b2a38d
Kalyani ramayane <ramayanekj2005@gmail.com>
2026-05-08 09:54:07 +0530

    updated audio files

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 
 AUDIO_RAW_DIR = os.path.join(DATA_DIR, "raw")
 AUDIO_PROCESSED_DIR = os.path.join(DATA_DIR, "processed")
-SEGMENTS_DIR = os.path.join(BASE_DIR, "segment_keySearch_summary")  # or "segment_keySearch_summary" if you want
+SEGMENTS_DIR = os.path.join(BASE_DIR, "audio_segment_keySearch_summary")  # or "segment_keySearch_summary" if you want
 TRANSCRIPTS_DIR = os.path.join(BASE_DIR, "transcripts")
 
 for d in [AUDIO_RAW_DIR, AUDIO_PROCESSED_DIR, SEGMENTS_DIR, TRANSCRIPTS_DIR]:
@@ -15,3 +15,5 @@ WHISPER_MODEL = "small"
 FP16 = False
 SENTENCES_PER_SEGMENT = 5
 MIN_MEDICAL_TERMS = 5
+
+
